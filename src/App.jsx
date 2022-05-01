c4a53434be09892041ad6bee1a48a5ef3a4e6f01
in.niko <inniko9111@gmail.com>
2022-05-01 21:43:17 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import Profile from './components/profile/Profile';
 import user from './data-files/user.json';
-
-
+import Statistics from './components/statistics/Statistics';
+import data from './data-files/data.json';
+import FriendList from './components/friendlist/FriendList';
+import friends from './data-files/friends.json';
+import TransactionHistory from './components/transactionhistory/TransactionHistory';
+import transactions from './data-files/transaction-history.json';
 
 function App() {
   return (
-  <div>
-   <Profile
-        username = {user.username}
-        tag = {user.tag}
-        location = {user.location}
-        avatar = {user.avatar}
-        stats = {user.stats} />
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </div>
   );
-};
+}
 
 export default App;
